e55df4aa351025ed1a7d9608654c6a3721a9e06f
AChapNoander <alexnoander14@gmail.com>
2017-03-26 21:17:27 -0400

    Submitting ToolBox

--- frequency.py
+++ frequency.py
@@ -2,15 +2,19 @@
 Project Gutenberg """
 
 import string
+import re
 
 
-def get_word_list(file_name):
+def get_word_list(file_name, n):
     """ Reads the specified project Gutenberg book.  Header comments,
     punctuation, and whitespace are stripped away.  The function
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
+    f = open(file_name, 'r')
+    text = f.read()
+    ls = re.compile('\w+').findall(text)
+    return(get_top_n_words(ls, n))
 
 
 def get_top_n_words(word_list, n):
@@ -23,8 +27,39 @@ def get_top_n_words(word_list, n):
     returns: a list of n most frequently occurring words ordered from most
     frequently to least frequentlyoccurring
     """
-    pass
+    words = []
+    frequency = []
+    for i in word_list:
+        i = str.lower(i)
+        if i not in words:
+            words.append(i)
+    for word in words:
+        freq = 0
+        for sample in word_list:
+            if word == sample:
+                freq = freq + 1
+        frequency.append(freq)
+
+    d = {}
+    for i, word in enumerate(words):
+        d[frequency[i]] = word
+    return sort_dictionary(d, n)
+
+
+def sort_dictionary(dictionary, n):
+    keys = dictionary.keys()
+    keys = sorted(keys)
+    word_list = []
+    for key in keys:
+        word_list.append(dictionary.get(key))
+    word_list = word_list[::-1]
+    word_list = word_list[:n]
+    return word_list
+
+
+
 
 if __name__ == "__main__":
     print("Running WordFrequency Toolbox")
     print(string.punctuation)
+    print(get_word_list('english.txt', 5))
